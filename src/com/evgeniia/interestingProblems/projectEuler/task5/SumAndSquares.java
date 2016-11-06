297ae92d56f6024bd5030f7be53b53459d89cbe9
Evgeniia Kim <kimjenya@gmail.com>
2016-11-06 09:37:21 -0500

    problem 6

---
+++ src/com/evgeniia/interestingProblems/projectEuler/task5/SumAndSquares.java
@@ -0,0 +1,28 @@
+package com.evgeniia.interestingProblems.projectEuler.task5;
+
+/**
+ * Created by evgeniia on 11/6/16.
+ */
+public class SumAndSquares {
+
+    public static long countSquareOfSum(int n) {
+        if (n <= 0) return 0;
+        long sum = (n * (n + 1)) / 2;
+        return sum * sum;
+    }
+
+    public static long countSumOfSquares(int n) {
+        if (n <= 0) return 0;
+        return (n * (n + 1) * (2 * n + 1)) / 6;
+    }
+
+    public static long countDifference(int n) {
+        return countSquareOfSum(n) - countSumOfSquares(n);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countDifference(100));
+    }
+
+
+}
---
+++ test/com/evgeniia/interestingProblems/projectEuler/task5/SumAndSquaresTest.java
@@ -0,0 +1,29 @@
+package com.evgeniia.interestingProblems.projectEuler.task5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by evgeniia on 11/6/16.
+ */
+public class SumAndSquaresTest {
+
+    @Test
+    public void countSumOfSquaresTest() {
+        assertEquals(5,SumAndSquares.countSumOfSquares(2));
+        assertEquals(0,SumAndSquares.countSumOfSquares(0));
+        assertEquals(0,SumAndSquares.countSumOfSquares(-4));
+        assertEquals(385,SumAndSquares.countSumOfSquares(10));
+
+    }
+
+    @Test
+    public void countSquareOfSumTest() {
+        assertEquals(9,SumAndSquares.countSquareOfSum(2));
+        assertEquals(0,SumAndSquares.countSquareOfSum(-2));
+        assertEquals(0,SumAndSquares.countSquareOfSum(0));
+        assertEquals(3025,SumAndSquares.countSquareOfSum(10));
+
+    }
+}
